3e7693ac6ba2c4da3c10874831c1127b3d7e74bc
madhumanti <madhumanti@madhumanti-HP-Notebook>
2020-01-17 23:39:24 +0530

    push

---
+++ src/main/java/iiitb/ss/calculator/calculator/App.java
@@ -0,0 +1,13 @@
+package iiitb.ss.calculator.calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
